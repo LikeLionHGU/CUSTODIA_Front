873eddabf20e5f23727dc3e37a0f43ad2e8e3316
jinsungkim <kimswim02@gmail.com>
2026-08-11 18:36:58 +0900

    feat:file update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import router from "./router";
+import router from "./router/index.jsx";
 import "./css/index.css";
 
 createRoot(document.getElementById("root")).render(
