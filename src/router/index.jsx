8f2924cd68c849dd6dc52b54e91ab5adb4f4cd63
jinsungkim <kimswim02@gmail.com>
2026-08-11 20:09:41 +0900

    edit:router default change

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Login />,
+        element: <Home />,
       },
       {
         path: "login",
